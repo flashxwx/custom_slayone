db1755ab437e93cbf9645b3de11194de2c0ddd74
flash <iamflashuwu@gmail.com>
2026-08-05 20:03:09 +0800

    Downloaded new official client codes

--- resource/main/dist/client-bundle.js
+++ resource/main/dist/client-bundle.js
@@ -5709,7 +5709,7 @@
                     var i = document.getElementById("serverDropdown").value;
                     (window.localStorage.setItem("chooseServerOnStart", i),
                       setTimeout(function () {
-                        window.location.reload();
+                        (0, r.reloadWithoutInviteLink)();
                       }, 250));
                   }
                   e.close();
@@ -23260,6 +23260,7 @@
           (exports.handleNetworkMsg =
             exports.Network =
             exports.networkInit =
+            exports.reloadWithoutInviteLink =
               void 0));
         var _1 = __webpack_require__(9994),
           servers_1 = __webpack_require__(6530),
@@ -23317,9 +23318,29 @@
             "server" === vals[0] &&
               (_forcedServerIndexInt = parseServerIndex(vals[1]));
         }
-        var _chosenServerIndex = window.localStorage.getItem(
-          "chooseServerOnStart",
+        var _chosenServerIndex = parseServerIndex(
+          window.localStorage.getItem("chooseServerOnStart"),
         );
+        function reloadWithoutInviteLink() {
+          var e = window.location.search
+              .substr(1)
+              .split("&")
+              .filter(function (e) {
+                return (
+                  e.length > 0 &&
+                  "game" !== e.split("=")[0] &&
+                  "server" !== e.split("=")[0]
+                );
+              }),
+            t =
+              window.location.origin +
+              window.location.pathname +
+              (e.length > 0 ? "?" + e.join("&") : "") +
+              window.location.hash;
+          t === window.location.href
+            ? window.location.reload()
+            : window.location.replace(t);
+        }
         function networkInit() {
           for (var e = 0; e < servers_1.SERVERS.length; e++)
             if (-1 === _forcedServerIndexInt || _forcedServerIndexInt === e)
@@ -23867,8 +23888,11 @@
           i.parentNode.removeChild(i);
         }
         (window.localStorage.setItem("chooseServerOnStart", "-1"),
-          -1 === _forcedServerIndexInt &&
-            (_forcedServerIndexInt = parseServerIndex(_chosenServerIndex)),
+          -1 !== _chosenServerIndex &&
+            (_chosenServerIndex !== _forcedServerIndexInt &&
+              (_forcedGameID = -1),
+            (_forcedServerIndexInt = _chosenServerIndex)),
+          (exports.reloadWithoutInviteLink = reloadWithoutInviteLink),
           (exports.networkInit = networkInit),
           (exports.Network = {
             lastPing: 0,
